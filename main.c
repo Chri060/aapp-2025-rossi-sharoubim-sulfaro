d9bf422420c64d48f47686daf0eeaf88eec47714
Rossi Christian <christian2.rossi@mail.polimi.it>
2024-10-16 13:04:22 +0200

    Added test suite

--- main.c
+++ main.c
@@ -9,19 +9,10 @@ int partition5(int* list, int left, int right);
 void swap(int* list, int i1, int i2);
 
 int main(void) {
-    int n, length;
-    int* list;
+    int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    const int length = sizeof(list) / sizeof(int);
+    const int n = 15;
 
-    printf("Index i: ");
-    scanf("%d", &n);
-    printf("Array length: ");
-    scanf("%d", &length);
-
-    list = (int*)malloc(length * sizeof(int));
-    for (int i = 0; i < length; i++) {
-        printf("Array element %d: ", i);
-        scanf("%d", &list[i]);
-    }
     const int index = select(list, 0, length - 1, n - 1);
     return list[index];
 }
